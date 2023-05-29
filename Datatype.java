15b8e783e117babfba5097be9665eba0883d5762
Prakashkvv <134916357+Prakashkvv@users.noreply.github.com>
2023-05-29 12:56:52 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Datatype {
+	
+	
+	public static void main(String[] args) {
+		int age = 35;
+		char name  = 'P';
+		float Height = 5.9f;
+		String Status = "I am becoming smarter";
+	
+		System.out.println(age + "/" + name);
+		System.out.println(name);
+		System.out.println(Height);
+		System.out.println(Status);
+		
+		
+		
+	}
+	
+	
+
+}
---
+++ IfForLoop.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class IfForLoop {
+
+	public static void main(String[] args) {
+		
+		// 1 to 20 ---> 0dd number
+		
+     for (int i = 1; i < 20; i++) {
+    	 
+    	 
+    	 // i%2==1, i%2!=0,  
+    	 
+    	 if (i%2==1) {
+    		 
+    		 System.out.println("Odd Number: " +i);
+			
+		}
+    	 
+    	 else {
+			
+    		 System.out.println("even number: " +i);
+		}
+		
+	}
+}
+}
